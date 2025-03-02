cb412b93754ce481e429e06a3718238581461ccb
ShahuSardar-py <shahu.sardar4@gmail.com>
2025-03-02 21:23:59 +0530

    tested AI feature

--- utils.py
+++ utils.py
@@ -1,12 +1,11 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-import streamlit as st
+
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
-st.set_page_config(page_title="hello")
 
 
 
@@ -15,4 +14,4 @@ response = client.models.generate_content(
     model="gemini-2.0-flash", 
     contents="Explain how AI works"
 )
-st.write(response)
+print(response)
